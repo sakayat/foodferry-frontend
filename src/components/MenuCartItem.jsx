7d5f102ea749a685b60dea5ecfa5c4d3d2637f20
sakayat <sakhyatkabir1@gmail.com>
2024-08-25 00:48:39 +0600

    updated cart componets

--- src/components/MenuCartItem.jsx
+++ src/components/MenuCartItem.jsx
@@ -3,6 +3,7 @@ import { Trash2, X } from "lucide-react";
 import CartInfo from "./CartInfo";
 
 const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
+  
   const token = localStorage.getItem("authToken");
 
   const [data, setData] = useState([]);
@@ -28,11 +29,11 @@ const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
 
   return (
     <div
-      className="cart-container absolute top-0 
-    right-0 w-[35rem]  bg-white p-4 z-10"
+      className="cart-container absolute top-0
+    right-0 w-[30rem] p-4 z-10"
     >
       <div className="space-y-5 min-h-[calc(100dvh-15rem)] flex flex-col justify-between">
-        <div className="border border-black rounded py-2 relative flex items-center justify-center">
+        <div className="border border-black/40 rounded py-2 relative flex items-center justify-center">
           <span className="font-semibold">Your Cart</span>
           <button
             className=" absolute right-0 px-4"
@@ -41,7 +42,7 @@ const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
             <X size={18} />
           </button>
         </div>
-        <div className="items border border-black rounded overflow-y-auto space-y-2 min-h-[calc(100dvh-15rem)]">
+        <div className="items border border-black/40 rounded overflow-y-auto space-y-2 min-h-[calc(100dvh-15rem)]">
           {data?.items?.map((item) => (
             <CartInfo key={item.id} item={item} />
           ))}
@@ -49,9 +50,9 @@ const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
         <div className="cart-footer border rounded py-3 px-4 space-y-5">
           <div className="cart-order-summary text-center space-x-4">
             <span>Subtotal:</span>
-            <span className="font-semibold">$ 396.00 USD</span>
+            <span className="font-semibold">{data.total_price} USD</span>
           </div>
-          <button className="py-2.5 rounded flex justify-center w-full default-btn">
+          <button className="py-2.5 flex justify-center w-full default-btn">
             View Cart
           </button>
         </div>
