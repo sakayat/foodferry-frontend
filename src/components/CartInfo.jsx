0ae3ed8020345da915b4bfbc144526ef2203b6fe
sakayat <sakhyatkabir1@gmail.com>
2024-08-25 10:27:06 +0600

    updated

--- src/components/CartInfo.jsx
+++ src/components/CartInfo.jsx
@@ -1,28 +1,82 @@
 import React, { useState } from "react";
-import { Trash2, X } from "lucide-react";
+import { Minus, Plus, Trash } from "lucide-react";
 
 const CartInfo = ({ item }) => {
+  const token = localStorage.getItem("authToken");
+  const [newData, setNewData] = useState(item);
   const [quantity, setQuantity] = useState(item.quantity);
 
+  const updateOnCart = async (quantity) => {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/cart/update/${item.id}/`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify({ quantity }),
+      }
+    );
+    const data = await res.json();
+    setNewData(data);
+  };
+
+  const handleChangeQuantity = (quantity) => {
+    updateOnCart(quantity);
+    setQuantity(quantity);
+  };
+
+  const handleChangePlusValue = () => {
+    handleChangeQuantity(quantity + 1);
+  };
+
+  const handleChangeMinusValue = () => {
+    if (quantity > 1) {
+      handleChangeQuantity(quantity - 1);
+    }
+  };
+
   return (
-    <div className="cart-product flex items-center justify-between w-full py-2 px-4">
-      <img
-        src={`${import.meta.env.VITE_API_BASE_URL}/media/${item.food_image}`}
-        alt=""
-        className="w-14 h-14 rounded"
-      />
-      <span>{item.food_item_name}</span>
-      <span>{item.total_price}</span>
-      <input
-        type="number"
-        name="number"
-        className="w-20 text-center border rounded-full py-1 px-4 border-black"
-        value={quantity}
-        onChange={(e) => setQuantity(e.target.value)}
-      />
-      <button className="bg-[#286140] text-white p-2.5 rounded-full">
-        <Trash2 size={18} />
-      </button>
+    <div className="cart-product space-y-5 w-full py-2 px-4">
+      <div className="flex items-center gap-5">
+        <img
+          src={`${import.meta.env.VITE_API_BASE_URL}/media/${
+            newData.food_image
+          }`}
+          alt=""
+          className="w-14 h-14 rounded"
+        />
+        <span>{newData.food_item_name}</span>
+        
+      </div>
+      <div className="flex items-center justify-between gap-5">
+      <span>{newData.total_price}</span>
+        <div className="flex items-center justify-between gap-2 border border-black p-2 w-36">
+          <div
+            className="text-2xl cursor-pointer"
+            onClick={handleChangeMinusValue}
+          >
+            <Minus size={18} />
+          </div>
+          
+          <input
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+            className="outline-none font-bold text-center w-6 [appearance:number] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+          />
+          <div
+            className="text-2xl cursor-pointer"
+            onClick={handleChangePlusValue}
+          >
+            <Plus size={18} />
+          </div>
+        </div>
+        <button className="bg-[#286140] text-white p-2.5 rounded-full">
+          <Trash size={18} />
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/MenuCartItem.jsx
+++ src/components/MenuCartItem.jsx
@@ -3,14 +3,14 @@ import { Trash2, X } from "lucide-react";
 import CartInfo from "./CartInfo";
 
 const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
-  
+
   const token = localStorage.getItem("authToken");
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchCartList();
-  }, []);
+  }, [data]);
 
   const fetchCartList = async () => {
     const res = await fetch(
@@ -27,16 +27,18 @@ const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
     setData(data);
   };
 
+  
+
   return (
     <div
       className="cart-container absolute top-0
     right-0 w-[30rem] p-4 z-10"
     >
-      <div className="space-y-5 min-h-[calc(100dvh-15rem)] flex flex-col justify-between">
+      <div className="space-y-5 min-h-[calc(100dvh-5rem)] flex flex-col justify-between">
         <div className="border border-black/40 rounded py-2 relative flex items-center justify-center">
           <span className="font-semibold">Your Cart</span>
           <button
-            className=" absolute right-0 px-4"
+            className="absolute right-0 px-4"
             onClick={() => setIsCartOpen(!isCartOpen)}
           >
             <X size={18} />
@@ -44,7 +46,11 @@ const MenuCartItem = ({ setIsCartOpen, isCartOpen }) => {
         </div>
         <div className="items border border-black/40 rounded overflow-y-auto space-y-2 min-h-[calc(100dvh-15rem)]">
           {data?.items?.map((item) => (
-            <CartInfo key={item.id} item={item} />
+            <CartInfo
+              key={item.id}
+              item={item}
+              setData={setData}
+            />
           ))}
         </div>
         <div className="cart-footer border rounded py-3 px-4 space-y-5">
