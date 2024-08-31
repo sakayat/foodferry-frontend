9655987a6c32ac0d6a7c6d748b8da0892ffe58ee
sakayat <sakhyatkabir1@gmail.com>
2024-08-31 12:51:13 +0600

    updated

--- src/components/SliderItem.jsx
+++ src/components/SliderItem.jsx
@@ -6,7 +6,9 @@ const SliderItem = ({ item }) => {
       <div className="grid grid-cols-12 gap-5">
         <div className="col-span-12 md:col-span-7 flex flex-col items-center md:items-start space-y-6">
           <span className="text-sm uppercase font-semibold">{item.text}</span>
-          <h5 className="home-subtitle text-6xl">{item.sub_title}</h5>
+          <h5 className="home-subtitle text-3xl text-center md:text-start">
+            {item.sub_title}
+          </h5>
           <h1 className="home-title text-5xl lg:text-8xl text-center md:text-start tracking-[0.5rem] uppercase font-bold">
             {item.title}
           </h1>
