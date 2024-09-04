0a57ee7a4c442cec8592cb24eff44f401887c801
sakayat <sakhyatkabir1@gmail.com>
2024-09-04 15:57:09 +0600

    added notfound components

--- src/pages/CategoryFoodPage.jsx
+++ src/pages/CategoryFoodPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { currencyFormat } from "../lib/utils";
+import NotFound from "../components/Notfound";
+
 
 const CategoryFoodPage = () => {
   const { slug } = useParams();
@@ -25,21 +27,31 @@ const CategoryFoodPage = () => {
     <div className="py-5">
       <div className="xl:container mx-auto px-8">
         <div className="py-5">
-          <h2 className="text-3xl capitalize">{slug}</h2>
+          {foodData.length > 0 && (
+            <h2 className="text-3xl capitalize">{slug}</h2>
+          )}
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          {foodData?.map((food) => (
-            <Link to={`/food/${food.slug}`} className="space-y-1" key={food.id}>
-              <img
-                src={`${import.meta.env.VITE_API_BASE_URL}/${food.image}`}
-                alt=""
-                className="h-44 w-full object-cover rounded-xl"
-              />
-              <h4 className="font-bold capitalize">{food.name}</h4>
-              <span className="price">{currencyFormat(food.price)}</span>
-            </Link>
-          ))}
-        </div>
+        {foodData.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {foodData?.map((food) => (
+              <Link
+                to={`/food/${food.slug}`}
+                className="space-y-1"
+                key={food.id}
+              >
+                <img
+                  src={`${import.meta.env.VITE_API_BASE_URL}/${food.image}`}
+                  alt=""
+                  className="h-44 w-full object-cover rounded-xl"
+                />
+                <h4 className="font-bold capitalize">{food.name}</h4>
+                <span className="price">{currencyFormat(food.price)}</span>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <NotFound />
+        )}
       </div>
     </div>
   );
